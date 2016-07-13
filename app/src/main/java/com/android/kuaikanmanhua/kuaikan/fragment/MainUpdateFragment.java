be12c28393a585e0d1933b7da7937483f349f445
mour471464900 <805256509@qq.com>
2016-07-14 00:38:17 +0800

    7月13日 0:23 完成 了 主页面的七天切换功能 更改了由于机型的问题造成的闪退 一个是匹配周几 一个匹配星期几

--- app/src/main/java/com/android/kuaikanmanhua/kuaikan/fragment/MainUpdateFragment.java
+++ app/src/main/java/com/android/kuaikanmanhua/kuaikan/fragment/MainUpdateFragment.java
@@ -111,7 +111,7 @@ public class MainUpdateFragment extends Fragment {
         Date date=new Date();
         String now = myFmt.format(date);
 //        匹配今天是星期几，然后动态加入星期几
-//        有的机型是周几
+//        有的机型是周几 ，三星的机型是星期几
         List<String> mList=new ArrayList<>();
         switch (now){
             case "星期一":
@@ -157,6 +157,7 @@ public class MainUpdateFragment extends Fragment {
             case "周日":
                 mList.addAll(getDay(day7));
                 break;
+//
         }
         return mList;
     }
