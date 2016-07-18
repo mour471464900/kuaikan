6f69191068f1ec38a08a0d9807d14d7430ba2268
mour471464900 <805256509@qq.com>
2016-07-18 17:41:27 +0800

    7月18日    17:40   完成观看漫画的acitivity界面，完成了分享功能的集成

--- app/src/main/java/com/android/kuaikanmanhua/kuaikan/activity/FullscreenActivity.java
+++ app/src/main/java/com/android/kuaikanmanhua/kuaikan/activity/FullscreenActivity.java
@@ -86,6 +86,8 @@ public class FullscreenActivity extends Activity implements View.OnClickListener
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullscreen);
+//         sharesdk的初始化要在oncreate方法 里面
+        ShareSDK.initSDK(FullscreenActivity.this);
         setupListView();
 //        设置listview
     }
@@ -291,7 +293,31 @@ public class FullscreenActivity extends Activity implements View.OnClickListener
                 break;
             case R.id.tv_full_share:
 //                跳转分享页面
-                setupShare();
+
+                OnekeyShare oks = new OnekeyShare();
+                //关闭sso授权
+                oks.disableSSOWhenAuthorize();
+
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+                //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+                // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+                oks.setTitle("快看漫画的分享");
+                // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+                oks.setTitleUrl("http://sharesdk.cn");
+                // text是分享文本，所有平台都需要这个字段
+                oks.setText("我是分享文本");
+                // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+                //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+                // url仅在微信（包括好友和朋友圈）中使用
+                oks.setUrl("http://sharesdk.cn");
+                // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+                oks.setComment("我是测试评论文本");
+                // site是分享此内容的网站名称，仅在QQ空间使用
+                oks.setSite(getString(R.string.app_name));
+                // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+                oks.setSiteUrl("http://sharesdk.cn");
+// 启动分享GUI
+                oks.show(FullscreenActivity.this);
                 Toast.makeText(FullscreenActivity.this, "点击了分享按钮", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.tv_more_comment:
@@ -305,35 +331,6 @@ public class FullscreenActivity extends Activity implements View.OnClickListener
         Intent intent =new Intent(this,MainReplyActivity.class);
         intent.putExtra("id",fullWatchBean.getData().getId());
         startActivity(intent);
-    }
-
-    //   分享的内容
-    private void setupShare() {
-        ShareSDK.initSDK(this);
-        OnekeyShare oks = new OnekeyShare();
-        //关闭sso授权
-        oks.disableSSOWhenAuthorize();
-
-// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
-        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
-        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
-        oks.setTitle("快看漫画");
-        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-        oks.setTitleUrl("http://sharesdk.cn");
-        // text是分享文本，所有平台都需要这个字段
-        oks.setText("我是分享文本");
-        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
-        // url仅在微信（包括好友和朋友圈）中使用
-        oks.setUrl("http://sharesdk.cn");
-        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-        oks.setComment("我是测试评论文本");
-        // site是分享此内容的网站名称，仅在QQ空间使用
-        oks.setSite(getString(R.string.app_name));
-        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-        oks.setSiteUrl("http://sharesdk.cn");
-// 启动分享GUI
-        oks.show(this);
     }
 
     //                跳转作者
